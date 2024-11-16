fca553013259d4e4a0438c73a33cd385dba8bafe
Bkarybaev <baielkarybaev@gmail.com>
2024-11-16 16:37:00 +0600

    method

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,104 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("---          Start game          ---\n");
+            System.out.print("Predict amount of points (2..12): ");
+            int UserNumber = scanner.nextInt();
+            System.out.println("User rolls the dices...");
+            if (UserNumber > 1 && UserNumber <= 12) {
+                gamePrint(UserNumber);
+            } else {
+                System.out.println("\n----=== write 2 to 12!!! ===---");
+            }
+            int computer = new Random().nextInt(2, 13);
+            int comNum = computer;
+            System.out.println("\n\nComputer predicted " + comNum + " points");
+            System.out.println("Computer rolls the dices...");
+            gamePrint(comNum);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+
         }
     }
+
+    public static String printDice(int numRandom) {
+        switch (numRandom) {
+            case 1 -> {
+                return """
+                        +-------+
+                        |       |
+                        |   #   |
+                        |       |
+                        +-------+
+                        """;
+            }
+            case 2 -> {
+                return """
+                        +-------+
+                        |   #   |
+                        |       |
+                        |   #   |
+                        +-------+
+                        """;
+            }
+            case 3 -> {
+                return """
+                        +-------+
+                        | #     |
+                        |   #   |
+                        |     # |
+                        +-------+
+                        """;
+            }
+            case 4 -> {
+                return """
+                        +-------+
+                        | #   # |
+                        |       |
+                        | #   # |
+                        +-------+
+                        """;
+            }
+            case 5 -> {
+                return """
+                        +-------+
+                        | #   # |
+                        |   #   |
+                        | #   # |
+                        +-------+
+                        """;
+            }
+            default -> {
+                return """
+                        +-------+
+                        | #   # |
+                        | #   # |
+                        | #   # |
+                        +-------+
+                        """;
+            }
+        }
+    }
+
+    public static int rollTheDice() {
+        return new Random().nextInt(6) + 1;
+    }
+
+    public static void gamePrint(int number) {
+        int runNum;
+        int increase = 0;
+        for (int i = 0; i < 2; i++) {
+            runNum = rollTheDice();
+            increase += runNum;
+            System.out.println(printDice(runNum));
+        }
+        System.out.println("On the dice fell " + increase + " points.");
+        int mathAbs = increase - Math.abs(increase - number) * 2;
+        System.out.println("Result is " + increase + " - " + "abs(" + increase + " - " + number + ")" + " * " + 2 + ": " + mathAbs + " points");
+    }
+
 }
